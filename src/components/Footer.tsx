fa75191e272eb9c774a0f4f92ef5127201b861fe
nengleangkhim <leangkhimneng11@gmail.com>
2025-02-22 10:26:54 +0700

    rebuild

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,6 +3,7 @@ const Footer: React.FC = () => {
       <footer id="footer" className="footer bg-black text-[#B48811] border-t-1 border-[#B48811] text-xl font-light py-20 text-center">
         <p>Contact: <br/>
           Email : neng.leangkhim99@gmail.com <br/>
+          Phone number : 087884141
           Phone number : 087884141</p>
       </footer>
     );
